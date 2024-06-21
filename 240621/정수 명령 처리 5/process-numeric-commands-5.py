ab4be82e81490feff3bd35fbba8e6cce1759b0dd
정지오 <104253583+jiohjung98@users.noreply.github.com>
2024-06-21 20:18:25 +0900

    Title: 정수 명령 처리 5, Time: 233ms, Memory: 26MB, Status: Wrong Answer - CodeTree

--- 240621/정수 명령 처리 5/process-numeric-commands-5.py
+++ 240621/정수 명령 처리 5/process-numeric-commands-5.py
@@ -1,5 +1,14 @@
 n = int(input())
+arr = []
 for _ in range(n):
     x = input()
     if 'push_back' in x:
-        print(1)
+        arr.append(int(x[10:12]))
+        # print(int(x[10:12]))
+    elif 'get' in x:
+        k, y = x.split( )
+        print(arr[int(y)-1])
+    elif x == 'size':
+        print(len(arr))
+    elif x == 'pop_back':
+        arr.pop()
