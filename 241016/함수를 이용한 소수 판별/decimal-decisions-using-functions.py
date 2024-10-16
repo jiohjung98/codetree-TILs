866638e2461e34175251e8486bce1c7c0b7fb94f
정지오 <104253583+jiohjung98@users.noreply.github.com>
2024-10-16 17:53:02 +0900

    Title: 함수를 이용한 소수 판별, Time: 110ms, Memory: 24MB, Status: Passed - CodeTree

--- 241016/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
+++ 241016/함수를 이용한 소수 판별/decimal-decisions-using-functions.py
@@ -1,7 +1,7 @@
 def is_prime(x):
+    if x < 2:
+        return False
     for i in range(2,x):
-        if i == 1:
-            return False
         if x % i == 0:
             return False
     return x
