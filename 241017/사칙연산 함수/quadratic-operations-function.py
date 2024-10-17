2a6406444038bb06c962f7efefa0d21ec2285e00
정지오 <104253583+jiohjung98@users.noreply.github.com>
2024-10-17 21:11:39 +0900

    Title: 사칙연산 함수, Time: 117ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241017/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,22 @@
+def add(x,y):
+    print(x,'+',y, '=', x+y);
+
+def sub(x,y):
+    print(x,'-',y, '=', x-y);
+
+def mul(x,y):
+    print(x,'*',y, '=', x*y);
+
+def div(x,y):
+    print(x,'/',y, '=', x//y);
+
+input_nums = list(map(str, input().split()))
+
+if input_nums[1] == '+':
+    div(int(input_nums[0]), int(input_nums[2]))
+elif input_nums[1] == '-':
+    sub(int(input_nums[0]), int(input_nums[2]))
+elif input_nums[1] == '*':
+    mul(int(input_nums[0]), int(input_nums[2]))
+else:
+    div(int(input_nums[0]), int(input_nums[2]))
