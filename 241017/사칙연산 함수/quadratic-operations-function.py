6150fd2c10c1d0d986dedbf31da22652628192ad
정지오 <104253583+jiohjung98@users.noreply.github.com>
2024-10-17 21:14:00 +0900

    Title: 사칙연산 함수, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 241017/사칙연산 함수/quadratic-operations-function.py
+++ 241017/사칙연산 함수/quadratic-operations-function.py
@@ -18,5 +18,7 @@ elif input_nums[1] == '-':
     sub(int(input_nums[0]), int(input_nums[2]))
 elif input_nums[1] == '*':
     mul(int(input_nums[0]), int(input_nums[2]))
-else:
+elif input_nums[1] == '/':
     div(int(input_nums[0]), int(input_nums[2]))
+else:
+    return False
