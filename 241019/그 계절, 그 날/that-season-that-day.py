791993d62221f2a7ee1c690ce2cc1cefcd85c0ba
정지오 <104253583+jiohjung98@users.noreply.github.com>
2024-10-19 00:06:57 +0900

    Title: 그 계절, 그 날, Time: 143ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241019/그 계절, 그 날/that-season-that-day.py
@@ -0,0 +1,33 @@
+def day_calc(x,y):
+    if y == 2:
+        if x % 4 == 0:
+            if x % 100 == 0:
+                return 28
+            elif x % 100 == 0 and x % 400 == 0:
+                return 29
+            else:
+                return 29
+        else:
+            return 28
+    if y == 4 or y == 6 or y == 9 or y == 11:
+        return 30
+    else:
+        return 31
+
+def season_calc(x,y,z):
+    maxDay = day_calc(x,y)
+    if maxDay < z:
+        print(-1)
+    else:
+        if 1 < y and y <= 3:
+            print('Winter')
+        elif 3 < y and y <= 6:
+            print('Spring')
+        elif 6 < y and y <= 9:
+            print('Summer')
+        else:
+            print('Fall')
+
+y, m, d = map(int, input().split())
+
+season_calc(y,m,d)
